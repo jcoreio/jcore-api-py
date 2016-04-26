026385cab3b0341cf92a12bcd4e60d59b32abba9
Andy Edwards <jedwards@fastmail.com>
2016-04-26 15:18:32 -0700

    initial commit

---
+++ jcore_api/api.py
@@ -0,0 +1,27 @@
+from websocket import create_connection
+
+from .api_common import LOCAL_SOCKET_PATH
+
+from base64 import b64decode
+import json
+
+# eyJ1cmwiOiJ3czovL2xvY2FsaG9zdDozMDMwL2pjb3JlLWFwaSIsInRva2VuIjoiRWxsOGpBd1NRcGd4d2RidkJDSXo4dGZqL2VWSE9nWnV2RGFVM1JxM0tZRnFZaXVYeWZDa1VnbTlQbmVINHQ5aCJ9
+
+def connect(apiToken):
+    """Connects to a jcore.io server.
+    """
+    assert type(apiToken) is str or type(apiToken) is unicode, 'apiToken must be a base64-encoded string or unicode'
+    assert len(apiToken) > 0, 'len(apiToken) must be > 0'
+
+    parsed = json.loads(b64decode(apiToken))
+    assert type(parsed) is dict, 'decoded apiToken must be a dict'
+
+    print parsed
+
+    url, token = parsed[u'url'], parsed[u'token']
+    assert type(url) is unicode, 'decoded url must be a unicode'
+    assert len(url) > 0, "len(<decoded apiToken>['url']) must be > 0"
+    assert type(token) is unicode, 'decoded token must be a unicode'
+    assert len(token) > 0, "len(<decoded apiToken>['token']) must be > 0"
+
+    sock = create_connection(url)
